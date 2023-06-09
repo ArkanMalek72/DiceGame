286f6c12ff18677271d08aff868075e3306da394
Arkan <arkanmalek.99@gmail.com>
2023-06-09 19:18:58 +0530

    edit files

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var randomdiceimagesource = "images/" + randomdiceimage;
 var image1  = document.querySelectorAll("img")[0];
 image1.setAttribute("src" , randomdiceimagesource);
 
+
 var randomnumber2 = Math.floor(Math.random() * 6) + 1;
 var randomimagesource2 = "images/dice" + randomnumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
+
 
 if(randomnumber1 > randomnumber2){
     document.querySelector("h1").innerHTML = "✌🏻 Player 1 Wins!";
@@ -15,3 +17,4 @@ if(randomnumber1 > randomnumber2){
 }else{
     document.querySelector("h1").innerHTML = "Draw!🔃";
 }
+var randomnumber1 = Math.random() 
